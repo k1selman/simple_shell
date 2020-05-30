f16df3fb945219b38015f9b5b45d9bf29562b581
Bartosz Kisiel <barkq113@gmail.com>
2020-05-30 15:17:41 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
 
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
@@ -13,10 +17,75 @@ int lsh_exit(char **args);
 
 //list of builtin commands, followed by their corresponding functions
 
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit",
+};
 
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit,
+};
 
+int lsh_num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
 
+//builtin function implementation
 
+int lsh_cd(char **args)
+{
+    if(args[1] == NULL){
+        fprintf(stderr, "lsh: EXPECTED ARGUMENT TO \"cd\"\n");
+    }else{
+        if(chdir(args[1]) != 0){
+            perror("lsh");
+        }
+    }
+
+    return 1;
+}
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Prototype \n");
+    printf("Type in program names and args, then hit enter\n");
+    printf("The following are built in: \n");
+
+    for(i = 0; i < lsh_num_builtins(); i++){
+        printf("%s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for info on other programs\n");
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
+}
+
+int lsh_execute(char **args)
+{
+    int i;
+
+    if(args[0] == NULL){
+        //empty command
+        return 1;
+    }
+
+    for(i = 0; i < lsh_num_builtins(); i++){
+        if(strcmp(args[0], builtin_str[i]) == 0){
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
+}
 
 
 char *lsh_read_line(void)
